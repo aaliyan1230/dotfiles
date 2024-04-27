6b292d49c597738eefeee467e3d63cb7dd6da19e
aaliyan1230 <aaliyan1230@gmail.com>
2024-04-27 23:11:49 +0500

    feat: package installation script updated and tested

--- packages.sh
+++ packages.sh
@@ -18,7 +18,16 @@ nvm install 18
 npm install -g yarn
 
 # Install development tools
-sudo apt install code # Visual Studio Code
+# Visual Studio Code
+sudo apt-get install wget gpg
+wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
+sudo install -D -o root -g root -m 644 packages.microsoft.gpg /etc/apt/keyrings/packages.microsoft.gpg
+sudo sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
+rm -f packages.microsoft.gpg
+sudo apt install apt-transport-https
+sudo apt update
+sudo apt install code 
+
 sudo apt install docker # Docker
 sudo apt install docker-compose # Docker Compose
 
@@ -33,9 +42,11 @@ source $HOME/.cargo/env
 sudo apt install g++
 
 # Install api tooling
-sudo apt install postman # Postman
+sudo snap install postman # Postman
 
 # Install pgAdmin
+sudo curl https://www.pgadmin.org/static/packages_pgadmin_org.pub | sudo apt-key add
+sudo sh -c 'echo "deb https://ftp.postgresql.org/pub/pgadmin/pgadmin4/apt/$(lsb_release -cs) pgadmin4 main" > /etc/apt/sources.list.d/pgadmin4.list && apt update'
 sudo apt install pgadmin4
 
 # Install gh ang ghcs ghce
@@ -43,6 +54,10 @@ sudo sudo apt install gh
 gh extension install github/copilot-cli
 
 # Install Google Chrome
-sudo apt install google-chrome-stable
+sudo apt update
+sudo sh -c 'echo "deb https://ftp.postgresql.org/pub/pgadmin/pgadmin4/apt/$(lsb_release -cs) pgadmin4 main" > /etc/apt/sources.list.d/pgadmin4.list && apt update'
+sudo dpkg -i google-chrome-stable_current_amd64.deb
+sudo apt-get install -f
+
 
 echo "Package installation complete!"
